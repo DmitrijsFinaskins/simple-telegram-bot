cadcdf57a1d99065988a535b8a529f1df8c904f9
DmitrijsF <dmitrijs@jugla.net>
2022-07-18 11:09:04 +0300

    main menu added

--- src/main/java/io/proj3ct/SpringDemoBot/service/TelegramBot.java
+++ src/main/java/io/proj3ct/SpringDemoBot/service/TelegramBot.java
@@ -5,9 +5,16 @@ import io.proj3ct.SpringDemoBot.config.BotConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import javax.xml.stream.events.Comment;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Component
@@ -15,8 +22,25 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     final BotConfig config;
 
+    static final String HELP_TEXT = "This bot is created to demonstrate Spring capabilities.\n\n" +
+            "You can execute commands from the main menu on the left or by typing a command:\n\n" +
+            "Type /start to see a welcome message\n\n" +
+            "Type /mydata to see data stored about yourself\n\n" +
+            "Type /help to see this message again";
+
     public TelegramBot(BotConfig config) {
         this.config = config;
+        List<BotCommand> listofCommands = new ArrayList<>();
+        listofCommands.add(new BotCommand("/start", "get a welcome message"));
+        listofCommands.add(new BotCommand("/mydata", "get your data stored"));
+        listofCommands.add(new BotCommand("/deletedata", "delete my data"));
+        listofCommands.add(new BotCommand("/help", "info how to use this bot"));
+        listofCommands.add(new BotCommand("/settings", "set your preferences"));
+        try {
+            this.execute(new SetMyCommands(listofCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error("Error setting bot's command list: " + e.getMessage());
+        }
     }
 
     @Override
@@ -32,7 +56,7 @@ public class TelegramBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
 
-        if(update.hasMessage() && update.getMessage().hasText()) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
             String messageText = update.getMessage().getText();
             long chatId = update.getMessage().getChatId();
 
@@ -40,38 +64,41 @@ public class TelegramBot extends TelegramLongPollingBot {
             switch (messageText) {
                 case "/start":
 
-                        startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
-                        break;
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+
+                case "/help":
+
+                    sendMessage(chatId, HELP_TEXT);
+                    break;
                 default:
 
-                        sendMessage(chatId, "Sorry, command was not recognized");
+                    sendMessage(chatId, "Sorry, command was not recognized");
 
             }
         }
 
 
-
     }
 
-    private void startCommandReceived(long chatId, String name){
+    private void startCommandReceived(long chatId, String name) {
 
 
-        String answer = "Hi, " + name +", nice to meet you!";
+        String answer = "Hi, " + name + ", nice to meet you!";
         log.info("Replied to user " + name);
 
 
         sendMessage(chatId, answer);
     }
 
-    private void sendMessage(long chatId, String textToSend)  {
+    private void sendMessage(long chatId, String textToSend) {
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
         message.setText(textToSend);
 
-        try{
+        try {
             execute(message);
-        }
-        catch (TelegramApiException e) {
+        } catch (TelegramApiException e) {
             log.error("Error occurred: " + e.getMessage());
         }
     }
